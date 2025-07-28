603b16e2a3b24e80518a5e0e15444af46120dce7
Clacky <develop@clacky.ai>
2025-07-28 05:39:06 -0400

    refactor: migrate pages from JSON to JSX and update build configuration
    Co-authored-by: Jalen Lee <li382112772@gmail.com>

---
+++ pages/_meta.js
@@ -0,0 +1,16 @@
+export default {
+  "index": "Introduction",
+  "another": "Another Page",
+  "sator12i": "sator12i Page",
+  "advanced": "Advanced (A Folder)",
+  "about": {
+    "title": "About",
+    "type": "page"
+  },
+  "contact": {
+    "title": "Contact ↗",
+    "type": "page",
+    "href": "https://twitter.com/shuding_",
+    "newWindow": true
+  }
+}
